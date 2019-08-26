a2eefa66d1d0a001ff18f7c0d2a6f05e44ef0999
horwengliang <horwengliang@gmail.com>
2019-08-26 18:32:15 +0900

    v0.0.43
    fix delete-products

--- server/graphql/resolvers/product/delete-products.ts
+++ server/graphql/resolvers/product/delete-products.ts
@@ -2,7 +2,7 @@ import { getRepository, In } from 'typeorm'
 import { Product } from '../../../entities'
 
 export const deleteProducts = {
-  async deleteCompanies(_: any, { names }) {
+  async deleteProducts(_: any, { names }) {
     await getRepository(Product).delete({
       name: In(names)
     })
