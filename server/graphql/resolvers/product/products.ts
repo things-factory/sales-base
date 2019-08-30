07ec89ac61bcfce1f15732c8d28ff83a2f81e81d
leemyunggyu <jaylee@hatiolab.com>
2019-08-30 17:50:42 +0900

    v0.0.56

--- server/graphql/resolvers/product/products.ts
+++ server/graphql/resolvers/product/products.ts
@@ -7,7 +7,7 @@ export const productsResolver = {
   async products(_: any, params: ListParam, context: any) {
     const convertedParams = convertListParams(params)
     const userBizplaces = await getUserBizplaces(context)
-    convertedParams.bizplace = In(userBizplaces.map(userBizplace => userBizplace.id))
+    convertedParams.where.bizplace = In(userBizplaces.map(userBizplace => userBizplace.id))
 
     const [items, total] = await getRepository(Product).findAndCount({
       ...convertedParams,
