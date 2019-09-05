08acbc4a0c36c5b0136bf21503dd60defb2565a0
leemyunggyu <jaylee@hatiolab.com>
2019-09-05 12:38:19 +0900

    v0.0.96

--- server/graphql/resolvers/arrival-notice/generate-arrival-notice.ts
+++ server/graphql/resolvers/arrival-notice/generate-arrival-notice.ts
@@ -1,42 +1,43 @@
-import { getManager } from 'typeorm'
+import { getManager, getRepository } from 'typeorm'
 import { ArrivalNotice, ArrivalNoticeProduct, ArrivalNoticeVas } from '../../../entities'
+import { createArrivalNotice } from './create-arrival-notice'
+import { create } from 'domain'
 
 export const generateArrivalNotice = {
-  async generateArrivalNotice(_: any, { data }, context: any) {
+  async generateArrivalNotice(_: any, { arrivalNotice }, context: any) {
     await getManager().transaction(async transactionalEntityManager => {
-      const { arrivalNotice, products, vass } = data
-      // create arrival notice product
+      const newArrivalNotice = arrivalNotice.arrivalNotice
+      const products = arrivalNotice.products
+      const vass = arrivalNotice.vass
+
+      // 1. Create arrival notice
+      const createdArrivalNotice: ArrivalNotice = await transactionalEntityManager.getRepository(ArrivalNotice).save({
+        domain: context.state.domain,
+        bizplace: context.state.bizplaces[0],
+        ...newArrivalNotice,
+        creator: context.state.user,
+        updater: context.state.user
+      })
+
+      // 2. Create arrival notice product
       products.forEach((product: ArrivalNoticeProduct) => {
         product.domain = context.state.domain
         product.name = `${product.batchId}-${product.seq}-${product.product.name}`
         product.creator = context.state.user
         product.updater = context.state.user
+        product.arrivalNotice = createdArrivalNotice
       })
-      const arrivalNoticeProducts: [ArrivalNoticeProduct] = await transactionalEntityManager
-        .getRepository(ArrivalNoticeProduct)
-        .save(products)
+      await transactionalEntityManager.getRepository(ArrivalNoticeProduct).save(products)
 
-      // create arrival notice vas
+      // 3. Create arrival notice vas
       vass.forEach((vas: ArrivalNoticeVas) => {
         vas.domain = context.state.domain
         vas.name = `${vas.batchId}-${vas.vas.name}`
         vas.creator = context.state.user
         vas.updater = context.state.user
+        vas.arrivalNotice = createdArrivalNotice
       })
-      const arrivalNoticeVass: [ArrivalNoticeVas] = await transactionalEntityManager
-        .getRepository(ArrivalNoticeVas)
-        .save(vass)
-
-      // create arrival notice
-      const createdArrivalNotice: ArrivalNotice = await transactionalEntityManager.getRepository(ArrivalNotice).save({
-        domain: context.state.domain,
-        bizplace: context.state.bizplaces[0],
-        ...arrivalNotice,
-        arrivalNoticeProducts,
-        arrivalNoticeVass,
-        creator: context.state.user,
-        updater: context.state.user
-      })
+      await transactionalEntityManager.getRepository(ArrivalNoticeVas).save(vass)
 
       return createdArrivalNotice
     })
