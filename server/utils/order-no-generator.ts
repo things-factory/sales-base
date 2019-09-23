32db6569f6b3d80091201d9745530290bcc71577
horwengliang <horwengliang@gmail.com>
2019-09-23 15:01:04 +0800

    v0.0.176
    update order no generator

--- server/utils/order-no-generator.ts
+++ server/utils/order-no-generator.ts
@@ -1,17 +1,18 @@
-const currentDate = new Date()
-
 export class OrderNoGenerator {
   static arrivalNotice() {
+    const currentDate = new Date()
     return `GAN-${currentDate.getFullYear()}${currentDate.getMonth() +
       1}${currentDate.getDate()}${currentDate.getHours()}${currentDate.getMinutes()}${currentDate.getSeconds()}${currentDate.getMilliseconds()}`
   }
 
   static collectionOrder() {
+    const currentDate = new Date()
     return `CO-${currentDate.getFullYear()}${currentDate.getMonth() +
       1}${currentDate.getDate()}${currentDate.getHours()}${currentDate.getMinutes()}${currentDate.getSeconds()}${currentDate.getMilliseconds()}`
   }
 
   static deliveryOrder() {
+    const currentDate = new Date()
     return `DO-${currentDate.getFullYear()}${currentDate.getMonth() +
       1}${currentDate.getDate()}${currentDate.getHours()}${currentDate.getMinutes()}${currentDate.getSeconds()}${currentDate.getMilliseconds()}`
   }
