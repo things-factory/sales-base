453d1a073a76f4133c8252e2d920f96c3155387c
horwengliang <horwengliang@gmail.com>
2019-09-23 12:44:56 +0800

    v0.0.175
    update order generator

--- server/utils/order-no-generator.ts
+++ server/utils/order-no-generator.ts
@@ -1,16 +1,19 @@
-import uuid from 'uuid/v4'
+const currentDate = new Date()
 
 export class OrderNoGenerator {
   static arrivalNotice() {
-    return uuid()
+    return `GAN-${currentDate.getFullYear()}${currentDate.getMonth() +
+      1}${currentDate.getDate()}${currentDate.getHours()}${currentDate.getMinutes()}${currentDate.getSeconds()}${currentDate.getMilliseconds()}`
   }
 
   static collectionOrder() {
-    return uuid()
+    return `CO-${currentDate.getFullYear()}${currentDate.getMonth() +
+      1}${currentDate.getDate()}${currentDate.getHours()}${currentDate.getMinutes()}${currentDate.getSeconds()}${currentDate.getMilliseconds()}`
   }
 
   static deliveryOrder() {
-    return uuid()
+    return `DO-${currentDate.getFullYear()}${currentDate.getMonth() +
+      1}${currentDate.getDate()}${currentDate.getHours()}${currentDate.getMinutes()}${currentDate.getSeconds()}${currentDate.getMilliseconds()}`
   }
 
   static orderProduct(orderName: String, productBatchId: String, productSeq: Number) {
