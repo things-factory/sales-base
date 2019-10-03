795824ff8022678caa35313bf3873225431c349d
leemyunggyu <jaylee@hatiolab.com>
2019-10-04 00:58:40 +0800

    v0.2.251

--- server/graphql/resolvers/arrival-notice/arrival-notice-requests.ts
+++ server/graphql/resolvers/arrival-notice/arrival-notice-requests.ts
@@ -1,6 +1,6 @@
 import { Bizplace } from '@things-factory/biz-base'
 import { convertListParams, ListParam } from '@things-factory/shell'
-import { getRepository, In, Equal, Not } from 'typeorm'
+import { getRepository, In, Not } from 'typeorm'
 import { ORDER_STATUS } from '../../../constants'
 import { ArrivalNotice } from '../../../entities'
 
@@ -9,7 +9,7 @@ export const arrivalNoticeRequestsResolver = {
     const convertedParams = convertListParams(params)
 
     if (!convertedParams.where || !convertedParams.where.status) {
-      convertedParams.where.status = Not(Equal(ORDER_STATUS.PENDING))
+      convertedParams.where.status = Not(In([ORDER_STATUS.PENDING, ORDER_STATUS.EDITING]))
     }
     convertedParams.bizplace = In(context.state.bizplaces.map((bizplace: Bizplace) => bizplace.id))
 
