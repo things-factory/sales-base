f8d9a0a84dff5c4e5f20cb78715935bb283af9cf
horwengliang <horwengliang@gmail.com>
2019-10-07 15:41:07 +0800

    v0.2.257

--- server/graphql/resolvers/collection-order/edit-collection-order.ts
+++ server/graphql/resolvers/collection-order/edit-collection-order.ts
@@ -1,13 +1,15 @@
 import { getManager, getRepository } from 'typeorm'
 import { ORDER_STATUS } from '../../../constants'
 import { CollectionOrder } from '../../../entities'
+import { schema } from '@things-factory/attachment-base'
 
 export const editCollectionOrder = {
-  async editCollectionOrder(_: any, { name, collectionOrder }, context: any) {
+  async editCollectionOrder(_: any, { name, collectionOrder, attachments }, context: any) {
     const foundCollectionOrder: CollectionOrder = await getRepository(CollectionOrder).findOne({
       where: { domain: context.state.domain, name },
       relations: ['creator', 'updater']
     })
+    const foundRefBy = foundCollectionOrder.id
 
     try {
       if (!foundCollectionOrder) throw new Error(`Collection order doesn't exists.`)
@@ -21,6 +23,15 @@ export const editCollectionOrder = {
           status: ORDER_STATUS.PENDING,
           updater: context.state.user
         })
+
+        // 2. Create attachment
+        if (!attachments) return updatedCollectionOrder
+        await schema.resolvers.mutations[0].deleteAttachmentsByRef(_, { foundRefBy }, context)
+
+        attachments = attachments.map(attachment => {
+          return { refBy: updatedCollectionOrder.id, file: attachment, category: 'ORDER' }
+        })
+        await schema.resolvers.mutations[0].createAttachments(_, { attachments }, context)
 
         return updatedCollectionOrder
       })
--- server/graphql/resolvers/delivery-order/edit-delivery-order.ts
+++ server/graphql/resolvers/delivery-order/edit-delivery-order.ts
@@ -1,13 +1,15 @@
 import { getManager, getRepository } from 'typeorm'
 import { ORDER_STATUS } from '../../../constants'
 import { DeliveryOrder } from '../../../entities'
+import { schema } from '@things-factory/attachment-base'
 
 export const editDeliveryOrder = {
-  async editDeliveryOrder(_: any, { name, deliveryOrder }, context: any) {
+  async editDeliveryOrder(_: any, { name, deliveryOrder, attachments }, context: any) {
     const foundDeliveryOrder: DeliveryOrder = await getRepository(DeliveryOrder).findOne({
       where: { domain: context.state.domain, name, status: ORDER_STATUS.EDITING },
       relations: ['creator', 'updater']
     })
+    const foundRefBy = foundDeliveryOrder.id
 
     try {
       if (!foundDeliveryOrder) throw new Error(`Delivery order doesn't exists.`)
@@ -21,6 +23,15 @@ export const editDeliveryOrder = {
           status: ORDER_STATUS.PENDING,
           updater: context.state.user
         })
+
+        // 2. Create attachment
+        if (!attachments) return updatedDeliveryOrder
+        await schema.resolvers.mutations[0].deleteAttachmentsByRef(_, { foundRefBy }, context)
+
+        attachments = attachments.map(attachment => {
+          return { refBy: updatedDeliveryOrder.id, file: attachment, category: 'ORDER' }
+        })
+        await schema.resolvers.mutations[0].createAttachments(_, { attachments }, context)
 
         return updatedDeliveryOrder
       })
