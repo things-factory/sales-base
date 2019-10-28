62c1bdb949b3209c52b8efd8f3e402a5fe9ff901
horwengliang <horwengliang@gmail.com>
2019-10-28 16:26:38 +0800

    v0.2.334

--- server/graphql/resolvers/delivery-order/delivery-order-requests.ts
+++ server/graphql/resolvers/delivery-order/delivery-order-requests.ts
@@ -1,21 +1,17 @@
 import { convertListParams, ListParam } from '@things-factory/shell'
 import { ORDER_STATUS } from '../../../constants'
-import { getRepository, In } from 'typeorm'
+import { getRepository, In, Not } from 'typeorm'
 import { DeliveryOrder } from '../../../entities'
+import { Bizplace } from '@things-factory/biz-base'
 
 export const deliveryOrderRequestsResolver = {
   async deliveryOrderRequests(_: any, params: ListParam, context: any) {
     const convertedParams = convertListParams(params)
 
-    if (!convertListParams.where || !convertListParams.where.status) {
-      convertedParams.where.status = In([
-        ORDER_STATUS.PENDING_RECEIVE,
-        ORDER_STATUS.READY_TO_DISPATCH,
-        ORDER_STATUS.DELIVERING,
-        ORDER_STATUS.REJECTED,
-        ORDER_STATUS.DONE
-      ])
+    if (!convertedParams.where || !convertedParams.where.status) {
+      convertedParams.where.status = Not(In([ORDER_STATUS.PENDING, ORDER_STATUS.EDITING]))
     }
+    convertedParams.bizplace = In(context.state.bizplaces.map((bizplace: Bizplace) => bizplace.id))
 
     const [items, total] = await getRepository(DeliveryOrder).findAndCount({
       ...convertedParams,
--- server/graphql/resolvers/release-good/release-good-requests.ts
+++ server/graphql/resolvers/release-good/release-good-requests.ts
@@ -1,22 +1,17 @@
 import { convertListParams, ListParam } from '@things-factory/shell'
 import { ORDER_STATUS } from '../../../constants'
-import { getRepository, In } from 'typeorm'
+import { getRepository, In, Not } from 'typeorm'
 import { ReleaseGood } from '../../../entities'
+import { Bizplace } from '@things-factory/biz-base'
 
 export const releaseGoodRequestsResolver = {
   async releaseGoodRequests(_: any, params: ListParam, context: any) {
     const convertedParams = convertListParams(params)
 
-    if (!convertListParams.where || !convertListParams.where.status) {
-      convertedParams.where.status = In([
-        ORDER_STATUS.PENDING_RECEIVE,
-        ORDER_STATUS.READY_TO_PICK,
-        ORDER_STATUS.INPROCESS,
-        ORDER_STATUS.DONE,
-        ORDER_STATUS.PROCESSING,
-        ORDER_STATUS.REJECTED
-      ])
+    if (!convertedParams.where || !convertedParams.where.status) {
+      convertedParams.where.status = Not(In([ORDER_STATUS.PENDING, ORDER_STATUS.EDITING]))
     }
+    convertedParams.bizplace = In(context.state.bizplaces.map((bizplace: Bizplace) => bizplace.id))
 
     const [items, total] = await getRepository(ReleaseGood).findAndCount({
       ...convertedParams,
