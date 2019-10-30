1c8b6cdf927d8393ee31882a757dd1f4b8779bb2
leemyunggyu <jaylee@hatiolab.com>
2019-10-30 15:33:26 +0900

    v0.2.335

--- server/graphql/resolvers/arrival-notice/generate-arrival-notice.ts
+++ server/graphql/resolvers/arrival-notice/generate-arrival-notice.ts
@@ -1,14 +1,32 @@
 import { Product } from '@things-factory/product-base'
-import { getManager } from 'typeorm'
+import { getManager, Not, Equal } from 'typeorm'
 import { ORDER_PRODUCT_STATUS, ORDER_STATUS, ORDER_TYPES, ORDER_VAS_STATUS } from '../../../constants'
 import { ArrivalNotice, OrderProduct, OrderVas, Vas } from '../../../entities'
 import { OrderNoGenerator } from '../../../utils/order-no-generator'
+import { Inventory } from '@things-factory/warehouse-base'
 
 export const generateArrivalNotice = {
   async generateArrivalNotice(_: any, { arrivalNotice }, context: any) {
     return await getManager().transaction(async trxMgr => {
       let orderProducts: OrderProduct[] = arrivalNotice.orderProducts
       let orderVass: OrderVas[] = arrivalNotice.orderVass
+
+      // Validation for duplication of batch id and product pair
+      // case 1. batch id is not duplicated => OK
+      // case 2. batch id is duplicated and product also same with previous one => OK
+      // case 3. batch id is duplicated but product is not same with previous one => Not OK
+      const duplicatedInventory: Inventory = await trxMgr.getRepository(Inventory).findOne({
+        where: orderProducts.map((orderProduct: OrderProduct) => {
+          return {
+            bizplace: context.state.mainBizplace,
+            domain: context.state.domain,
+            batchId: orderProduct.batchId,
+            product: Not(Equal(orderProduct.product))
+          }
+        })
+      })
+
+      if (duplicatedInventory) throw new Error(`Batch id is duplicated. (${duplicatedInventory.batchId})`)
 
       // 1. Create arrival notice
       const createdArrivalNotice: ArrivalNotice = await trxMgr.getRepository(ArrivalNotice).save({
