0d80e74274ed91ebe7a7a080507c9bf6408d28f4
farishelmi <farishelmi@hatiolab.com>
2019-11-15 16:18:19 +0800

    update GRN

---
+++ server/graphql/resolvers/goods-receival-note/customer-receival-notes.ts
@@ -0,0 +1,18 @@
+import { ListParam, convertListParams } from '@things-factory/shell'
+import { getRepository } from 'typeorm'
+import { GoodsReceivalNote } from '../../../entities/goods-receival-note'
+
+export const customerReceivalNotesResolver = {
+  async customerReceivalNotes(_: any, params: ListParam, context: any) {
+    const convertedParams = convertListParams(params)
+    const [items, total] = await getRepository(GoodsReceivalNote).findAndCount({
+      ...convertedParams,
+      where: {
+        domain: context.state.domain,
+        bizplace: context.state.mainBizplace,
+      },
+      relations: ['domain', 'arrivalNotice', 'bizplace', 'creator', 'updater']
+    })
+    return { items, total }
+  }
+}
--- server/graphql/resolvers/goods-receival-note/index.ts
+++ server/graphql/resolvers/goods-receival-note/index.ts
@@ -1,5 +1,6 @@
 import { goodsReceivalNoteResolver } from './goods-receival-note'
 import { goodsReceivalNotesResolver } from './goods-receival-notes'
+import { customerReceivalNotesResolver } from './customer-receival-notes'
 
 import { updateGoodsReceivalNote } from './update-goods-receival-note'
 import { createGoodsReceivalNote } from './create-goods-receival-note'
@@ -9,7 +10,8 @@ import { generateGoodsReceivalNote } from './generate-goods-receival-note'
 
 export const Query = {
   ...goodsReceivalNotesResolver,
-  ...goodsReceivalNoteResolver
+  ...goodsReceivalNoteResolver,
+  ...customerReceivalNotesResolver
 }
 
 export const Mutation = {
