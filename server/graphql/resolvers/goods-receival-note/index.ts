b5d747482a38a7e6a64fb542542deaa53d3ab8eb
leemyunggyu <jaylee@hatiolab.com>
2019-11-28 11:43:12 +0900

    v0.9.18

--- server/graphql/resolvers/goods-receival-note/index.ts
+++ server/graphql/resolvers/goods-receival-note/index.ts
@@ -6,7 +6,7 @@ import { updateGoodsReceivalNote } from './update-goods-receival-note'
 import { createGoodsReceivalNote } from './create-goods-receival-note'
 import { deleteGoodsReceivalNote } from './delete-goods-receival-note'
 import { deleteGoodsReceivalNotes } from './delete-goods-receival-notes'
-import { generateGoodsReceivalNote } from './generate-goods-receival-note'
+import { generateGoodsReceivalNoteResolver } from './generate-goods-receival-note'
 
 export const Query = {
   ...goodsReceivalNotesResolver,
@@ -19,5 +19,5 @@ export const Mutation = {
   ...createGoodsReceivalNote,
   ...deleteGoodsReceivalNote,
   ...deleteGoodsReceivalNotes,
-  ...generateGoodsReceivalNote
+  ...generateGoodsReceivalNoteResolver
 }
