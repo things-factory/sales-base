cdcf6eb2c1d7b3aec6ac4d004fc734fac83c4e4f
horwengliang <horwengliang@gmail.com>
2020-01-07 15:45:46 +0900

    update d/o

--- server/graphql/resolvers/delivery-order/dispatch-delivery-order.ts
+++ server/graphql/resolvers/delivery-order/dispatch-delivery-order.ts
@@ -1,34 +1,42 @@
-import { TransportDriver, TransportVehicle } from "@things-factory/transport-base";
-import { getManager } from "typeorm";
-import { ORDER_STATUS } from "../../../constants";
-import { DeliveryOrder } from "../../../entities";
-import { OrderNoGenerator } from "../../../utils";
+import { getManager } from 'typeorm'
+import { ORDER_STATUS } from '../../../constants'
+import { DeliveryOrder } from '../../../entities'
+import { TransportDriver, TransportVehicle, TRUCK_STATUS } from '@things-factory/transport-base'
 
 export const dispatchDeliveryOrder = {
   async dispatchDeliveryOrder(_: any, { orderInfo }, context: any) {
     return await getManager().transaction(async trxMgr => {
       try {
-        const foundDeliveryOrder: DeliveryOrder = await trxMgr
-          .getRepository(DeliveryOrder)
-          .findOne({
-            where: { domain: context.state.domain, name: orderInfo.name }
-          });
+        const foundDeliveryOrder: DeliveryOrder = await trxMgr.getRepository(DeliveryOrder).findOne({
+          where: { domain: context.state.domain, name: orderInfo.name },
+          relations: ['transportVehicle']
+        })
 
-        if (!foundDeliveryOrder)
-          throw new Error(`Delivery order doesn't exists.`);
-        if (foundDeliveryOrder.status !== ORDER_STATUS.READY_TO_DISPATCH)
-          throw new Error(`Status is not receivable.`);
+        if (!foundDeliveryOrder) throw new Error(`Delivery order doesn't exists.`)
+        if (foundDeliveryOrder.status !== ORDER_STATUS.READY_TO_DISPATCH) throw new Error(`Status is not receivable.`)
+
+        const truck = foundDeliveryOrder.transportVehicle
+        await trxMgr.getRepository(TransportVehicle).save({
+          ...truck,
+          status: TRUCK_STATUS.IN_USE,
+          updater: context.state.user
+        })
 
         await trxMgr.getRepository(DeliveryOrder).save({
           ...foundDeliveryOrder,
+          transportDriver: await trxMgr.getRepository(TransportDriver).findOne({
+            where: { domain: context.state.domain, name: orderInfo.driverName }
+          }),
+          to: orderInfo.to,
+          deliveryDate: orderInfo.deliveryDate,
           status: ORDER_STATUS.DELIVERING,
           updater: context.state.user
-        });
+        })
 
-        return foundDeliveryOrder;
+        return foundDeliveryOrder
       } catch (e) {
-        throw e;
+        throw e
       }
-    });
+    })
   }
-};
+}
