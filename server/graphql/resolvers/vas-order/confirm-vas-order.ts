3b6980b677859e8a8f7c26d05b66fae2b70428e7
horwengliang <horwengliang@gmail.com>
2020-01-08 14:09:51 +0900

    fix notification

--- server/graphql/resolvers/vas-order/confirm-vas-order.ts
+++ server/graphql/resolvers/vas-order/confirm-vas-order.ts
@@ -37,6 +37,7 @@ export const confirmVasOrder = {
             .select('role.id')
             .from(Role, 'role')
             .where("role.name = 'Office Admin'")
+            .andWhere('role.domain_id = :domain', { domain: context.state.domain.id })
             .getQuery()
           return 'ur.roles_id IN ' + subQuery
         })
