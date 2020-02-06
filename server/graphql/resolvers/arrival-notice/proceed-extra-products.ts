dc3061e2de1e3abc6ca0030290caa181a3f924ac
leemyunggyu <jaylee@hatiolab.com>
2020-02-06 15:33:33 +0900

    v2.2.0-beta.5

--- server/graphql/resolvers/arrival-notice/proceed-extra-products.ts
+++ server/graphql/resolvers/arrival-notice/proceed-extra-products.ts
@@ -1,17 +1,20 @@
-import { ArrivalNotice, OrderProduct } from '../../../entities'
-import { ORDER_STATUS, ORDER_PRODUCT_STATUS } from '../../../constants/order'
+import { Role } from '@things-factory/auth-base'
+import { Bizplace, getMyBizplace } from '@things-factory/biz-base'
+import { sendNotification } from '@things-factory/shell'
 import { EntityManager, getManager } from 'typeorm'
-import { getMyBizplace } from '@things-factory/biz-base'
+import { ORDER_PRODUCT_STATUS, ORDER_STATUS } from '../../../constants/order'
+import { ArrivalNotice, OrderProduct } from '../../../entities'
 
 export const proceedExtraProductsResolver = {
   async proceedExtraProducts(_: any, { ganNo, approvedProducts, rejectedProducts }, context: any): Promise<void> {
     return await getManager().transaction(async (trxMgr: EntityManager) => {
       // Validation
       // Check status of GAN
+      const customerBizplace: Bizplace = await getMyBizplace(context.state.user)
       const arrivalNotice: ArrivalNotice = await trxMgr.getRepository(ArrivalNotice).findOne({
         where: {
           domain: context.state.domain,
-          bizplace: await getMyBizplace(context.state.user),
+          bizplace: customerBizplace,
           name: ganNo
         },
         relations: ['orderProducts']
@@ -41,6 +44,39 @@ export const proceedExtraProductsResolver = {
       await trxMgr
         .getRepository(OrderProduct)
         .delete(rejectedProducts.map((rejectedProd: OrderProduct) => rejectedProd.id))
+
+      // notification logics
+      // get Office Admin Users
+      const users: any[] = await trxMgr
+        .getRepository('users_roles')
+        .createQueryBuilder('ur')
+        .select('ur.users_id', 'id')
+        .where(qb => {
+          const subQuery = qb
+            .subQuery()
+            .select('role.id')
+            .from(Role, 'role')
+            .where("role.name = 'Office Admin'")
+            .andWhere('role.domain_id = :domain', { domain: context.state.domain.id })
+            .getQuery()
+          return 'ur.roles_id IN ' + subQuery
+        })
+        .getRawMany()
+
+      // send notification to Office Admin Users
+      if (users?.length) {
+        const msg = {
+          title: `Extra products approved/rejected by ${customerBizplace.name}`,
+          message: `Newly approved products are ready to unloading`,
+          url: context.header.referer
+        }
+        users.forEach(user => {
+          sendNotification({
+            receiver: user.id,
+            message: JSON.stringify(msg)
+          })
+        })
+      }
     })
   }
 }
