69f43d0e565a40e45d0b7257c93367e6de70c416
leemyunggyu <jaylee@hatiolab.com>
2020-03-27 17:46:25 +0900

    v2.4.0-beta.3

--- server/graphql/resolvers/goods-receival-note/generate-goods-receival-note.ts
+++ server/graphql/resolvers/goods-receival-note/generate-goods-receival-note.ts
@@ -1,32 +1,41 @@
 import { User } from '@things-factory/auth-base'
 import { Bizplace } from '@things-factory/biz-base'
 import { Domain } from '@things-factory/shell'
-import { getManager, getRepository } from 'typeorm'
+import { EntityManager, getManager, getRepository, Repository } from 'typeorm'
+import { GRN_STATUS } from '../../../constants'
 import { ArrivalNotice } from '../../../entities/arrival-notice'
 import { GoodsReceivalNote } from '../../../entities/goods-receival-note'
 import { OrderNoGenerator } from '../../../utils/order-no-generator'
-import { GRN_STATUS } from '../../../constants'
 
 export const generateGoodsReceivalNoteResolver = {
   async generateGoodsReceivalNote(_: any, { grn }, context: any) {
-    return await getManager().transaction(async trxMgr => {
-      return await generateGoodsReceivalNote(grn, context.state.domain, context.state.user)
+    return await getManager().transaction(async (trxMgr: EntityManager) => {
+      return await generateGoodsReceivalNote(grn, context.state.domain, context.state.user, trxMgr)
     })
   }
 }
 
-export async function generateGoodsReceivalNote(grn: any, domain: Domain, user: User): Promise<GoodsReceivalNote> {
+export async function generateGoodsReceivalNote(
+  grn: { refNo: string; customer: string },
+  domain: Domain,
+  user: User,
+  trxMgr?: EntityManager
+): Promise<GoodsReceivalNote> {
   // 1. Create grn
-  return await getRepository(GoodsReceivalNote).save({
+  const grnRepo: Repository<GoodsReceivalNote> =
+    trxMgr?.getRepository(GoodsReceivalNote) || getRepository(GoodsReceivalNote)
+  const bizplaceRepo: Repository<Bizplace> = trxMgr?.getRepository(Bizplace) || getRepository(Bizplace)
+  const ganRepo: Repository<ArrivalNotice> = trxMgr?.getRepository(ArrivalNotice) || getRepository(ArrivalNotice)
+
+  const bizplace: Bizplace = await bizplaceRepo.findOne({ where: { id: grn.customer } })
+  const arrivalNotice: ArrivalNotice = await ganRepo.findOne({ where: { domain, name: grn.refNo } })
+
+  return await grnRepo.save({
     ...grn,
     name: OrderNoGenerator.goodsReceiveNote(),
     domain,
-    bizplace: await getRepository(Bizplace).findOne({
-      where: { id: grn.customer }
-    }),
-    arrivalNotice: await getRepository(ArrivalNotice).findOne({
-      where: { domain, name: grn.refNo }
-    }),
+    bizplace,
+    arrivalNotice,
     status: GRN_STATUS.PENDING_PROCESS,
     creator: user,
     updater: user
