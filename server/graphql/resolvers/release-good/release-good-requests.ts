444852f9f175ad29d43e7e104bef76d4ee90cd88
ChrisLim <chrislim@hatiolab.com>
2020-04-17 14:25:39 +0800

    Update release-good-requests.ts

--- server/graphql/resolvers/release-good/release-good-requests.ts
+++ server/graphql/resolvers/release-good/release-good-requests.ts
@@ -1,39 +1,67 @@
 import { getPermittedBizplaceIds, Bizplace } from '@things-factory/biz-base'
-import { convertListParams, ListParam } from '@things-factory/shell'
-import { getRepository, In, Not, IsNull } from 'typeorm'
+import { convertListParams, ListParam, buildQuery } from '@things-factory/shell'
+import { getRepository, In, Not, IsNull, SelectQueryBuilder } from 'typeorm'
 import { ORDER_STATUS } from '../../../constants'
 import { ReleaseGood } from '../../../entities'
 
 export const releaseGoodRequestsResolver = {
   async releaseGoodRequests(_: any, params: ListParam, context: any) {
-    const convertedParams = convertListParams(params)
+    try {
+      const statusFilter = params.filters.some((e) => e.name === 'status')
+      const bizplaceFilter = params.filters.find((param) => param.name === 'bizplaceId')
 
-    if (!convertedParams.where || !convertedParams.where.status) {
-      convertedParams.where.status = Not(In([ORDER_STATUS.PENDING, ORDER_STATUS.EDITING]))
-    }
-
-    const bizplaceParam = params.filters.find((param) => param.name === 'bizplaceName')
-    if (bizplaceParam) {
-      const foundBizplaces: Bizplace[] = await getRepository(Bizplace).find({
-        where: {
-          ...convertListParams({ filters: [{ ...bizplaceParam, name: 'name' }] }).where,
-          bizplace: In(await getPermittedBizplaceIds(context.state.domain, context.state.user)),
-        },
-      })
-      if (foundBizplaces && foundBizplaces.length) {
-        convertedParams.where.bizplace = In(foundBizplaces.map((foundBizplace: Bizplace) => foundBizplace.id))
-      } else {
-        convertedParams.where.bizplace = IsNull()
+      if (!statusFilter) {
+        params.filters.push({
+          name: 'status',
+          operator: 'notin',
+          value: [ORDER_STATUS.PENDING, ORDER_STATUS.EDITING],
+          relation: false,
+        })
       }
-    } else {
-      convertedParams.where.bizplace = In(await getPermittedBizplaceIds(context.state.domain, context.state.user))
+
+      if (!bizplaceFilter) {
+        params.filters.push({
+          name: 'bizplaceId',
+          operator: 'in',
+          value: await getPermittedBizplaceIds(context.state.domain, context.state.user),
+          relation: false,
+        })
+      }
+
+      const qb: SelectQueryBuilder<ReleaseGood> = getRepository(ReleaseGood).createQueryBuilder('rg')
+      buildQuery(qb, params, context)
+      qb.addSelect((subQuery) => {
+        return subQuery
+          .select('COALESCE("ccd".rank, 99999)', 'rank')
+          .from('common_code_details', 'ccd')
+          .innerJoin('ccd.commonCode', 'cc')
+          .where('"ccd"."name" = "rg"."status"')
+          .andWhere('"ccd"."domain_id" = "rg"."domain_id"')
+          .andWhere('"cc"."name" = \'RO_REQUESTS_STATUS\'')
+      }, 'rank')
+      qb.leftJoinAndSelect('rg.domain', 'domain')
+      qb.leftJoinAndSelect('rg.bizplace', 'bizplace')
+      qb.leftJoinAndSelect('rg.creator', 'creator')
+      qb.leftJoinAndSelect('rg.updater', 'updater')
+
+      const arrChildSortData = ['bizplace']
+      const sort = (params.sortings || []).reduce(
+        (acc, sort) => ({
+          ...acc,
+          [arrChildSortData.indexOf(sort.name) >= 0 ? sort.name + '.name' : 'rg.' + sort.name]: sort.desc
+            ? 'DESC'
+            : 'ASC',
+        }),
+        !params.sortings.some((e) => e.name === 'status') ? { rank: 'ASC' } : {}
+      )
+
+      qb.orderBy(sort)
+
+      const [items, total] = await qb.getManyAndCount()
+
+      return { items, total }
+    } catch (error) {
+      throw error
     }
-
-    const [items, total] = await getRepository(ReleaseGood).findAndCount({
-      ...convertedParams,
-      relations: ['domain', 'bizplace', 'orderInventories', 'orderVass', 'shippingOrder', 'creator', 'updater'],
-    })
-
-    return { items, total }
   },
 }
