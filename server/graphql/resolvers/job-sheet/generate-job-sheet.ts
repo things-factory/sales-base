b0aa4157517762ba67c0830e4d1bdaf778e5ae8a
horwengliang <horwengliang@gmail.com>
2020-05-15 10:45:03 +0800

    fixed bizplace

--- server/graphql/resolvers/job-sheet/generate-job-sheet.ts
+++ server/graphql/resolvers/job-sheet/generate-job-sheet.ts
@@ -1,34 +1,33 @@
 import { User } from '@things-factory/auth-base'
-import { Bizplace, getMyBizplace } from '@things-factory/biz-base'
+import { Bizplace } from '@things-factory/biz-base'
 import { Domain } from '@things-factory/shell'
 import { EntityManager, getManager, getRepository, Repository } from 'typeorm'
 import { JobSheet } from '../../../entities'
 import { OrderNoGenerator } from '../../../utils/order-no-generator'
 
 export const generateJobSheetResolver = {
-  async generateJobSheet(_: any, { containerInfo }, context: any) {
+  async generateJobSheet(_: any, { myBizplace, containerInfo }, context: any) {
     return await getManager().transaction(async trxMgr => {
-      return await generateJobSheet(context.state.domain, context.state.user, containerInfo, trxMgr)
+      return await generateJobSheet(context.state.domain, context.state.user, myBizplace, containerInfo, trxMgr)
     })
   }
 }
 
 export async function generateJobSheet(
-  containerInfo: any,
   domain: Domain,
   user: User,
+  containerInfo: any,
+  myBizplace: Bizplace,
   trxMgr?: EntityManager
 ): Promise<JobSheet> {
   const jobSheetRepo: Repository<JobSheet> = trxMgr ? trxMgr.getRepository(JobSheet) : getRepository(JobSheet)
-
-  const myBizplace: Bizplace = await getMyBizplace(user)
 
   // 1. Create job sheet
   const createdJobSheet: JobSheet = await jobSheetRepo.save({
     name: OrderNoGenerator.jobSheet(domain.name),
     domain,
     bizplace: myBizplace,
-    adviseMtDate: containerInfo.containerMtDate,
+    adviseMtDate: containerInfo.mtDate,
     containerSize: containerInfo.containerSize,
     creator: user,
     updater: user
