ad166f6801378fd3685ed22d806f036784d5278d
horwengliang <horwengliang@gmail.com>
2020-07-15 12:27:18 +0800

    update job sheet patch resolver

--- server/graphql/resolvers/job-sheet/update-job-sheet.ts
+++ server/graphql/resolvers/job-sheet/update-job-sheet.ts
@@ -1,5 +1,5 @@
 import { getManager } from 'typeorm'
-import { JobSheet } from '../../../entities'
+import { JobSheet, ArrivalNotice } from '../../../entities'
 
 export const updateJobSheet = {
   async updateJobSheet(_, { name, patch }, context: any) {
@@ -9,6 +9,19 @@ export const updateJobSheet = {
           domain: context.state.domain,
           name
         }
+      })
+
+      const foundGAN: ArrivalNotice = await trxMgr.getRepository(ArrivalNotice).findOne({
+        where: {
+          domain: context.state.domain,
+          jobSheet
+        }
+      })
+
+      await trxMgr.getRepository(ArrivalNotice).save({
+        ...foundGAN,
+        ata: patch.ata,
+        updater: context.state.user
       })
 
       return await trxMgr.getRepository(JobSheet).save({
--- server/graphql/types/job-sheet/job-sheet-patch.ts
+++ server/graphql/types/job-sheet/job-sheet-patch.ts
@@ -3,6 +3,7 @@ import { gql } from 'apollo-server-koa'
 export const JobSheetPatch = gql`
   input JobSheetPatch {
     name: String
+    ata: String
     description: String
     adviseMtDate: String
     containerMtDate: String
