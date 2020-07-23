603a8b87d80aaea9c72d93cb57553667baae670c
Joe Kin <joekin95@gmail.com>
2020-07-23 10:27:34 +0800

    fixed lock qty calculation

--- server/graphql/resolvers/release-good/reject-release-good.ts
+++ server/graphql/resolvers/release-good/reject-release-good.ts
@@ -31,8 +31,8 @@ export const rejectReleaseGood = {
 
                   await trxMgr.getRepository(Inventory).save({
                     ...oi.inventory,
-                    lockedQty: oi.inventory.qty - oi.releaseQty,
-                    lockedWeight: oi.inventory.weight - oi.releaseWeight,
+                    lockedQty: oi.inventory.lockedQty - oi.releaseQty,
+                    lockedWeight: oi.inventory.lockedWeight - oi.releaseWeight,
                     updater: context.state.user
                   })
                 }
