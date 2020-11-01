a0f58a1787638ecc2282a3b696a305ce9f9ba947
horwengliang <horwengliang@gmail.com>
2020-11-01 16:51:03 +0800

    [D/O] add sorting based on common-codes ranking

--- server/graphql/resolvers/delivery-order/delivery-orders.ts
+++ server/graphql/resolvers/delivery-order/delivery-orders.ts
@@ -1,63 +1,74 @@
-import { getPermittedBizplaceIds, Bizplace } from '@things-factory/biz-base'
-import { convertListParams, ListParam } from '@things-factory/shell'
-import { getRepository, In, IsNull } from 'typeorm'
-import { Attachment } from '@things-factory/attachment-base'
+import { getPermittedBizplaceIds } from '@things-factory/biz-base'
+import { buildQuery, convertListParams, ListParam } from '@things-factory/shell'
+import { getRepository, SelectQueryBuilder } from 'typeorm'
 import { DeliveryOrder, ReleaseGood } from '../../../entities'
 
 export const deliveryOrdersResolver = {
   async deliveryOrders(_: any, params: ListParam, context: any) {
-    const convertedParams = convertListParams(params)
+    const bizplaceFilter = params.filters.find(param => param.name === 'bizplaceId')
+    if (!bizplaceFilter) {
+      params.filters.push({
+        name: 'bizplaceId',
+        operator: 'in',
+        value: await getPermittedBizplaceIds(context.state.domain, context.state.user),
+        relation: false
+      })
+    }
 
     const releaseGoodParam = params.filters.find(param => param.name === 'releaseGoodNo')
     if (releaseGoodParam) {
       let arrFilters = []
-      if (releaseGoodParam) arrFilters.push({ ...releaseGoodParam, name: 'name' })
+      params.filters.splice(
+        params.filters.findIndex(item => item.name == 'releaseGoodNo'), 1
+      )
+      arrFilters.push({ ...releaseGoodParam, name: 'name' })
+
       const foundReleaseGoods: ReleaseGood[] = await getRepository(ReleaseGood).find({
         ...convertListParams({ filters: arrFilters })
       })
+
       if (foundReleaseGoods && foundReleaseGoods.length) {
-        convertedParams.where.releaseGood = In(foundReleaseGoods.map((foundRG: ReleaseGood) => foundRG.id))
-      } else {
-        convertListParams.where.releaseGood = IsNull()
-      }
-    }
-
-    const bizplaceParam = params.filters.find(param => param.name === 'bizplaceName')
-    if (bizplaceParam) {
-      const foundBizplaces: Bizplace[] = await getRepository(Bizplace).find({
-        where: {
-          ...convertListParams({ filters: [{ ...bizplaceParam, name: 'name' }] }).where,
-          bizplace: In(await getPermittedBizplaceIds(context.state.domain, context.state.user))
-        }
-      })
-      if (foundBizplaces && foundBizplaces.length) {
-        convertedParams.where.bizplace = In(foundBizplaces.map((foundBizplace: Bizplace) => foundBizplace.id))
-      } else {
-        convertedParams.where.bizplace = IsNull()
-      }
-    } else {
-      convertedParams.where.bizplace = In(await getPermittedBizplaceIds(context.state.domain, context.state.user))
-    }
-
-    let [items, total] = await getRepository(DeliveryOrder).findAndCount({
-      ...convertedParams,
-      relations: ['domain', 'bizplace', 'releaseGood', 'transportDriver', 'transportVehicle', 'creator', 'updater']
-    })
-
-    items = await Promise.all(
-      items.map(async item => {
-        const foundAttachments = await getRepository(Attachment).find({
-          where: {
-            domain: context.state.domain,
-            refBy: item.id
-          }
+        params.filters.push({
+          name: 'releaseGoodId',
+          operator: 'in',
+          value: foundReleaseGoods.map((releaseGood: ReleaseGood) => releaseGood.id),
+          relation: false
         })
-        return {
-          ...item,
-          attachments: foundAttachments
-        }
-      })
+      }
+    }
+
+    const qb: SelectQueryBuilder<DeliveryOrder> = getRepository(DeliveryOrder).createQueryBuilder('do')
+    buildQuery(qb, params, context)
+    qb.addSelect(subQuery => {
+      return subQuery
+        .select('COALESCE("ccd".rank, 99999)', 'rank')
+        .from('common_code_details', 'ccd')
+        .innerJoin('ccd.commonCode', 'cc')
+        .where('"ccd"."name" = "do"."status"')
+        .andWhere('"ccd"."domain_id" = "do"."domain_id"')
+        .andWhere('"cc"."name" = \'DELIVERY_STATUS\'')
+    }, 'rank')
+    qb.leftJoinAndSelect('do.domain', 'domain')
+    qb.leftJoinAndSelect('do.bizplace', 'bizplace')
+    qb.leftJoinAndSelect('do.releaseGood', 'rg')
+    qb.leftJoinAndSelect('do.transportDriver', 'td')
+    qb.leftJoinAndSelect('do.transportVehicle', 'th')
+    qb.leftJoinAndSelect('do.creator', 'creator')
+    qb.leftJoinAndSelect('do.updater', 'updater')
+
+    const arrChildSortData = ['bizplace', 'releaseGood']
+    const sort = (params.sortings || []).reduce(
+      (acc, sort) => ({
+        ...acc,
+        [arrChildSortData.indexOf(sort.name) >= 0 ? sort.name + '.name' : 'do.' + sort.name]: sort.desc
+          ? 'DESC'
+          : 'ASC'
+      }),
+      !params.sortings.some(e => e.name === 'status') ? { rank: 'ASC' } : {}
     )
+
+    qb.orderBy(sort)
+    const [items, total] = await qb.getManyAndCount()
 
     return { items, total }
   }
