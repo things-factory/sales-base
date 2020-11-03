0252c9741c6ba06e8377767606c275a7d596ce42
horwengliang <horwengliang@gmail.com>
2020-11-03 22:57:49 +0800

    [D/O] overwrite remark in D/O

--- server/graphql/resolvers/delivery-order/dispatch-delivery-order.ts
+++ server/graphql/resolvers/delivery-order/dispatch-delivery-order.ts
@@ -1,17 +1,38 @@
 import { getManager } from 'typeorm'
 import { ORDER_STATUS } from '../../../constants'
-import { DeliveryOrder } from '../../../entities'
+import { DeliveryOrder, OrderInventory } from '../../../entities'
+import { Domain } from '@things-factory/shell'
+import { User } from '@things-factory/auth-base'
 import { ContactPoint } from '@things-factory/biz-base'
 import { TransportDriver, TransportVehicle, TRUCK_STATUS } from '@things-factory/transport-base'
+import { Inventory } from '@things-factory/warehouse-base'
+import { Product } from '@things-factory/product-base'
 
 export const dispatchDeliveryOrder = {
-  async dispatchDeliveryOrder(_: any, { orderInfo }, context: any) {
+  async dispatchDeliveryOrder(_: any, { orderInfo, orderItems }, context: any) {
     return await getManager().transaction(async trxMgr => {
       try {
+        const { domain, user }: { domain: Domain; user: User } = context.state
         const foundDeliveryOrder: DeliveryOrder = await trxMgr.getRepository(DeliveryOrder).findOne({
-          where: { domain: context.state.domain, name: orderInfo.name },
+          where: { domain, name: orderInfo.name, status: ORDER_STATUS.READY_TO_DISPATCH },
           relations: ['transportVehicle', 'bizplace']
         })
+
+        let orderInventories: OrderInventory[]
+        orderInventories = await trxMgr.getRepository(OrderInventory).find({
+          where: { domain, deliveryOrder: foundDeliveryOrder },
+          relations: ['inventory', 'inventory.product']
+        })
+
+        orderInventories = orderInventories.map((orderInventory: OrderInventory)=> {
+          const inventory: Inventory = orderInventory.inventory
+          const product: Product = inventory.product
+          const foundItem = orderItems.filter((item: any) => item.productName === `${product.name} (${product.description})`)
+          
+          if (foundItem[0].remark !== '') orderInventory.remark = foundItem[0].remark
+          return orderInventory
+        })
+        await trxMgr.getRepository(OrderInventory).save(orderInventories)
 
         if (!foundDeliveryOrder) throw new Error(`Delivery order doesn't exists.`)
         if (foundDeliveryOrder.status !== ORDER_STATUS.READY_TO_DISPATCH) throw new Error(`Status is not receivable.`)
@@ -22,16 +43,16 @@ export const dispatchDeliveryOrder = {
         let foundCP: any = {}
         if (orderInfo?.otherDestination) {
           foundCP = {
-            domain: context.state.domain,
+            domain,
             name: orderInfo.contactName,
             bizplace: foundDeliveryOrder.bizplace,
             address: orderInfo.otherDestination,
-            creator: context.state.user
+            creator: user
           }
           foundCP = await trxMgr.getRepository(ContactPoint).save(foundCP)
         } else {
           foundCP = await trxMgr.getRepository(ContactPoint).findOne({
-            where: { domain: context.state.domain, id: orderInfo.contactPoint }
+            where: { domain, name: orderInfo.contactPoint, bizplace: foundDeliveryOrder.bizplace }
           })
         }
 
@@ -39,19 +60,19 @@ export const dispatchDeliveryOrder = {
         let transportVehicle: TransportVehicle = null
         if (orderInfo?.ownDriver) {
           transportDriver = await trxMgr.getRepository(TransportDriver).findOne({
-            where: { domain: context.state.domain, name: orderInfo.ownDriver }
+            where: { domain, name: orderInfo.ownDriver }
           })
         }
 
         if (orderInfo?.ownTruck) {
           transportVehicle = await trxMgr.getRepository(TransportVehicle).findOne({
-            where: { domain: context.state.domain, name: orderInfo.ownTruck }
+            where: { domain, name: orderInfo.ownTruck }
           })
 
           await trxMgr.getRepository(TransportVehicle).save({
             ...transportVehicle,
             status: TRUCK_STATUS.IN_USE,
-            updater: context.state.user
+            updater: user
           })
         }
 
@@ -64,9 +85,10 @@ export const dispatchDeliveryOrder = {
           truckNo: orderInfo?.otherTruck || orderInfo?.ownTruck,
           to: foundCP.address,
           deliveryDate: orderInfo.deliveryDate,
+          remark: orderInfo.remark,
           status: ORDER_STATUS.DELIVERING,
           reusablePallet: orderInfo.reusablePallet,
-          updater: context.state.user
+          updater: user
         })
 
         return foundDeliveryOrder
